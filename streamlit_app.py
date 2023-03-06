cc731d680df48d4e531182899a5d7cd9b6380414
Shyam Krishnankutty <symk004@gmail.com>
2023-03-06 07:26:14 +0530

    Update streamlit_app.py
    added items.
    changed header.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Health Dinner')
+streamlit.title('My Mom's New Health Dinner')
 streamlit.header('Breakfast and Favorites')
 streamlit.text('Omega3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
