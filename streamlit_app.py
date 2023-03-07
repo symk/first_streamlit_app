c394eb52e0884176dd38e336797aadde1f941aae
Shyam Krishnankutty <symk004@gmail.com>
2023-03-07 08:34:36 +0530

    Update streamlit_app.py
    Let's removed the line of raw JSON, and separate the base URL from the fruit name (which will make it easier to use a variable there).

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-streamlit.text(fruityvice_response.json()) #writes the data to teh screen
+----##streamlit.text(fruityvice_response.json()) #writes the data to teh screen
 
 #take the ason verion of thr repoence and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
