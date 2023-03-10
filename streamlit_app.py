fbce9fb0de99fbdf91b08cb71b4b650dc35e1e65
Shyam Krishnankutty <symk004@gmail.com>
2023-03-10 11:47:28 +0530

    Update streamlit_app.py
    After testing the SQL in Snowflake, copy it into your streamlit file and change the word "test" to "from streamlit" so you know where the row comes from. 
    
    control of flow

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,3 +57,6 @@ streamlit.dataframe(my_data_rows)
 streamlit.header("Fruityvice Fruit Advice!")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', add_my_fruit)
+
+streamlit.write('Thanks for adding ' 'add_my_fruit')
+my_cur.execute("Insert into FRUIT_LOAD_LIST values ('from_streamlit')")
