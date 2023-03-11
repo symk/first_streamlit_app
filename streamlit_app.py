2d6a8ad9fa9854e1805b01fd2be30b54e4542b6a
Shyam Krishnankutty <symk004@gmail.com>
2023-03-11 19:05:47 +0530

    Update streamlit_app.py
    🥋 Move the Fruit Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,18 +59,24 @@ streamlit.stop()
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit Load List Contains")
-streamlit.dataframe(my_data_rows)
+##my_cur = my_cnx.cursor()
+##my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit Load List Contains :")
+#snoflake related fucntion
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+       my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+       retrun my_cur.fetchall()
+#Add a Button to the load list
+if streamlit.button('Get fruit load list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows =get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 
 
